7996ffa5499042aba469217ef550969a6280272d
RampeK <rkuorikoski13@gmail.com>
2025-02-22 13:17:14 +0200

    Mutsis update

--- Softlock/Assets/Scripts/BoxDestroy.cs
+++ Softlock/Assets/Scripts/BoxDestroy.cs
@@ -3,13 +3,54 @@ using UnityEngine;
 public class BoxDestroy : MonoBehaviour
 {
     [SerializeField] private float destroyY = -10f;    // Y-koordinaatti jonka alla laatikko tuhoutuu
+    [SerializeField] private float softLockTime = 1f;  // Aika jonka laatikon pitää olla nurkassa
+    
+    private float softLockTimer = 0f;
+    private int wallContactCount = 0;  // Montaa seinää laatikko koskee
     
     private void Update()
     {
         CheckDestroy();
+        CheckSoftLock();
     }
     
-    // Tarkista pitääkö laatikko tuhota
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            wallContactCount++;
+        }
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            wallContactCount--;
+        }
+    }
+    
+    private void CheckSoftLock()
+    {
+        // Soft lock tapahtuu vain kun laatikko koskee kahta tai useampaa seinää (nurkka)
+        bool isInCorner = wallContactCount >= 2;
+        
+        if (isInCorner)
+        {
+            softLockTimer += Time.deltaTime;
+            
+            if (softLockTimer >= softLockTime)
+            {
+                Debug.Log("Box is softlocked in corner!");
+                Destroy(gameObject); //TODO: Korvaa end screenillä
+            }
+        }
+        else
+        {
+            softLockTimer = 0f;
+        }
+    }
+    
     private void CheckDestroy()
     {
         if (transform.position.y < destroyY)
